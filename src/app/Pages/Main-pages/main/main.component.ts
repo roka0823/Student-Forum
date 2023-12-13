ea71913804b5ba24cf97cee145bcde58f22e9fed
Szentgyörgyhegyi Roland <roland.szentgyorgyhegyi@4ig.hu>
2023-12-13 18:25:14 +0100

    Main page improvements

--- src/app/Pages/Main-pages/main/main.component.ts
+++ src/app/Pages/Main-pages/main/main.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, ChangeDetectorRef} from '@angular/core';
 import {User} from "../../../Shared/Models/User";
 import {UserService} from "../../../Shared/Services/User-services/user.service";
 import {PostsService} from "../../../Shared/Services/Posts-service/posts.service";
 import {Post} from "../../../Shared/Models/Post";
+import {Subject} from "../../../Shared/Models/Subject";
+import {SubjectService} from "../../../Shared/Services/Subject-services/subject.service";
 
 @Component({
   selector: 'app-main',
@@ -12,11 +14,15 @@ import {Post} from "../../../Shared/Models/Post";
 export class MainComponent implements OnInit {
   public user!: User;
   public posts: Post[] = [];
+  public subjects: Subject[] = [];
 
   constructor(private userService: UserService,
-              private postService: PostsService) {}
+              private postService: PostsService,
+              private subjectService: SubjectService,
+              private cdRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    this.loadSubjects();
     this.loadUser();
     this.getNews();
   }
@@ -27,20 +33,27 @@ export class MainComponent implements OnInit {
     });
   }
 
-  getNews() {
+  loadSubjects() {
+    this.subjectService.getAllSubjects().subscribe( subjects => {
+      console.log(subjects)
+      this.subjects = subjects;
+      this.cdRef.detectChanges();
+    });
+  }
+
+  getNews(): void {
     this.postService.getPosts().subscribe(posts => {
       this.posts = posts;
 
-      // Assuming this.user.subjects is an array of subjects for the current user
       if (this.user && this.user.subjects) {
         this.posts = this.posts.filter(post => this.user.subjects.includes(post.subject));
       }
-
       this.posts.sort((a, b) => (b.time.toDate() as any) - (a.time.toDate() as any));
     });
   }
 
-  logPost() {
-    console.log(this.posts);
+
+  getSubject(subjectId: string): Subject | undefined {
+    return this.subjects.find((subject) => subject.id === subjectId);
   }
 }
